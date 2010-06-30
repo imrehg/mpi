bbf96e34ae11c308cae91cd4056393d8216b1de9
Gergely Imreh <imrehg@gmail.com>
2010-06-30 11:49:13 +0800

    picalc: set number of clients on the command line

--- picalc/master.py
+++ picalc/master.py
@@ -3,8 +3,9 @@ from mpi4py import MPI
 import numpy
 import sys
 
+clients = int(sys.argv[1]) if len(sys.argv) > 1 else 5
+print "Clients: %d" %clients
 start = MPI.Wtime()
-clients = 2
 comm = MPI.COMM_SELF.Spawn(sys.executable,
                            args=['cpi.py'],
                            maxprocs=clients)
